2e98e48ec115bf6e7a12f676b22c35ab8cf37b62
vlad-km <gm5461@gmail.com>
2018-04-03 07:37:25 +0300

    clearance normal.lisp

--- normal.lisp
+++ normal.lisp
@@ -18,36 +18,6 @@
 ;;;; Top-Level Functions
 
 
-#|
-(defun mklist (x)
-    "If x is a list, return it; otherwise return a singleton list, (x)."
-    (if (listp x) x (list x)))
-|#
-
-
-#|
-(defun ->cnf (p &optional vars)
-    "Convert a sentence p to conjunctive normal form [p 279-280]."
-    ;; That is, return (and (or ...) ...) where
-    ;; each of the conjuncts has all literal disjuncts.
-    ;; VARS is a list of universally quantified variables that P is in scope of.
-    (setf p (eliminate-implications (logic p)))
-    (case (op p)
-      (NOT (let ((p2 (move-not-inwards (arg1 p))))
-               (if (literal-clause? p2) p2 (->cnf p2 vars))))
-      (AND (conjunction (mappend #'(lambda (q) (conjuncts (->cnf q vars)))
-                                 (args p))))
-      (OR  (merge-disjuncts (mapcar #'(lambda (q) (->cnf q vars))
-                                    (args p))))
-      (FORALL (let ((new-vars (mapcar #'new-variable  (ensure-list (arg1 p)))))
-                  (->cnf (sublis (mapcar #'cons  (ensure-list (arg1 p)) new-vars)
-                                 (arg2 p))
-                         (append new-vars vars))))
-      (EXISTS (->cnf (skolemize (arg2 p) (arg1 p) vars) vars))
-      (t   p) ; p is atomic
-      ))
-|#
-
 (defun ->cnf (p &optional vars)
     "Convert a sentence p to conjunctive normal form [p 279-280]."
     ;; That is, return (and (or ...) ...) where
@@ -71,26 +41,6 @@
     "Convert a sentence p to implicative normal form [p 282]."
     (conjunction (mapcar #'cnf1->inf1 (conjuncts (->cnf p)))))
 
-
-(defun ->inf- (p)
-    "Convert a sentence p to implicative normal form [p 282]."
-    (let ((inf))
-        (dolist (it (conjuncts (->cnf p)) (conjunction (reverse inf)))
-            (push (cnf1->inf1 it) inf))))
-
-
-
-
-#|
-(defun ->horn (p)
-    "Try to convert sentence to a Horn clause, or a conjunction of Horn clauses.
-  Signal an error if this cannot be done."
-    (let ((q (->inf p)))
-        (when (not (every #'horn-clause? (conjuncts q)))
-            (warn "~A, converted to ~A, is not a Horn clause." p q))
-        q))
-|#
-
 (defun ->horn (p)
     "Try to convert sentence to a Horn clause, or a conjunction of Horn clauses.
   Signal an error if this cannot be done."
@@ -99,16 +49,8 @@
             (error "~A, converted to ~A, is not a Horn clause." p q))
         q))
 
-
-#|
 (defun logic (sentence)
-    "Canonicalize a sentence into proper logical form."
-    (cond ((stringp sentence) (->prefix sentence))
-          (t sentence)))
-|#
-
-(defun logic (sentence)
-    "Canonicalize a sentence into proper logical form."
+    "Canonicalize a sentence not implemented"
     (cond ((stringp sentence) (error "Canonicalize isnt release ~a" sentence))
           (t sentence)))
 
@@ -125,7 +67,6 @@
         `(=> ,(conjunction lhs) ,(disjunction rhs))))
 
 (defun eliminate-implications (p)
-    ;;(print (list 'eliminate p))
     (if (literal-clause? p)
         p
         (case (intern (symbol-name (op p)) :horn)
@@ -133,23 +74,8 @@
           (<=> `(and (or ,(arg1 p) (not ,(arg2 p)))
                      (or (not ,(arg1 p)) ,(arg2 p))))
           (t
-           ;;(print (list 'tcons 'op (op p)))
            (cons (op p) (mapcar #'eliminate-implications (args p)))))))
 
-#|
-(defun move-not-inwards (p)
-    "Given P, return ~P, but with the negation moved as far in as possible."
-    (case (op p)
-      (TRUE 'false)
-      (FALSE 'true)
-      (NOT (arg1 p))
-      (AND (disjunction (mapcar #'move-not-inwards (args p))))
-      (OR  (conjunction (mapcar #'move-not-inwards (args p))))
-      (FORALL (make-exp 'EXISTS (arg1 p) (move-not-inwards (arg2 p))))
-      (EXISTS (make-exp 'FORALL (arg1 p) (move-not-inwards (arg2 p))))
-      (t (make-exp 'not p))))
-|#
-
 (defun move-not-inwards (p)
     "Given P, return ~P, but with the negation moved as far in as possible."
     (case (op p)
@@ -160,23 +86,6 @@
       (OR  (conjunction (mapcar #'move-not-inwards (args p))))
       (t (make-exp 'not p))))
 
-
-#|
-(defun merge-disjuncts (disjuncts)
-    "Return a CNF expression for the disjunction."
-    ;; The argument is a list of disjuncts, each in CNF.
-    ;; The second argument is a list of conjuncts built so far.
-    (case (length disjuncts)
-      (0 'false)
-      (1 (first disjuncts))
-      (t (conjunction
-          (let ((result nil))
-              (for each y in (conjuncts (merge-disjuncts (rest disjuncts))) do
-                  (for each x in (conjuncts (first disjuncts)) do
-                      (push (disjunction (append (disjuncts x) (disjuncts y)))
-                            result)))
-              (nreverse result))))))
-|#
 
 (defun merge-disjuncts (disjuncts)
     "Return a CNF expression for the disjunction."
@@ -194,27 +103,9 @@
               (nreverse result))))))
 
 
-#|
-(defun skolemize (p vars outside-vars)
-    "Within the proposition P, replace each of VARS with a skolem constant,
-  or if OUTSIDE-VARS is non-null, a skolem function of them."
-    (sublis (mapcar #'(lambda (var)
-                          (cons var (if (null outside-vars)
-                                        (skolem-constant var)
-                                        (cons (skolem-constant var) outside-vars))))
-                    (ensure-list vars))
-            p))
-
-(defun skolem-constant (name)
-    "Return a unique skolem constant, a symbol starting with '$'."
-    (intern (format nil "$~A_~D" name (incf *new-variable-counter*))))
-
-|#
-
 (defun renaming? (p q &optional (bindings +no-bindings+))
     "Are p and q renamings of each other? (That is, expressions that differ
   only in variable names?)"
-    ;;(print (list 'renaming p q bindings))
     (cond ((eq bindings +fail+) +fail+)
           ((equal p q) bindings)
           ((and (consp p) (consp q))
@@ -232,22 +123,6 @@
 ;;;; Utility Predicates and Accessors
 
 (defconstant +logical-connectives+ '(and or not => <=>))
-;;;(defconstant +logical-quantifiers+ '(forall exists))
-
-#|
-(defun atomic-clause? (sentence)
-    "An atomic clause has no connectives or quantifiers."
-    (not (or (member (op sentence) +logical-connectives+)
-             (member (op sentence) +logical-quantifiers+))))
-|#
-
-
-#|
-(defun atomic-clause? (sentence)
-    "An atomic clause has no connectives or quantifiers."
-    (not (or (member (intern (symbol-name (op sentence)) :horn) +logical-connectives+)
-             (member (intern (symbol-name (op sentence)) :horn) +logical-quantifiers+))))
-|#
 
 (defun atomic-clause? (sentence)
     "An atomic clause has no connectives or quantifiers."
@@ -258,12 +133,6 @@
     "A literal is an atomic clause or a negated atomic clause."
     (or (atomic-clause? sentence)
         (and (negative-clause? sentence) (atomic-clause? (arg1 sentence)))))
-
-#|
-(defun negative-clause? (sentence)
-    "A negative clause has NOT as the operator."
-    (eq (op sentence) 'not))
-|#
 
 (defun negative-clause? (sentence)
     "A negative clause has NOT as the operator."
@@ -278,37 +147,12 @@
      (every #'atomic-clause? (conjuncts (arg1 sentence)))
      (atomic-clause? (arg2 sentence))))
 
-#|
-(defun conjuncts (sentence)
-    "Return a list of the conjuncts in this sentence."
-    (cond ((eq (intern (symbol-name (op sentence)) :horn) 'and) (args sentence))
-          ((eq sentence 'true) nil)
-          (t (list sentence))))
-|#
-
 (defun conjuncts (sentence)
     "Return a list of the conjuncts in this sentence."
     (cond ((eq (intern (symbol-name (op sentence)) :horn) 'and) (args sentence))
           ((symbolp sentence)
            (if (eq (intern (symbol-name sentence) :horn) 'true) nil))
           (t (list sentence))))
-
-
-#|
-(defun disjuncts (sentence)
-    "Return a list of the disjuncts in this sentence."
-    (cond ((eq (op sentence) 'or) (args sentence))
-          ((eq sentence 'false) nil)
-          (t (list sentence))))
-|#
-
-#|
-(defun disjuncts (sentence)
-    "Return a list of the disjuncts in this sentence."
-    (cond ((eq (intern (symbol-name (op sentence)) :horn) 'or) (args sentence))
-          ((eq sentence 'false) nil)
-          (t (list sentence))))
-|#
 
 (defun disjuncts (sentence)
     "Return a list of the disjuncts in this sentence."
