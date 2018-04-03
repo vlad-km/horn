eeb3a790b18174732bc8e23f94791b913266a374
vlad-km <gm5461@gmail.com>
2018-04-03 07:40:06 +0300

    clearance utilities.lisp

--- utilities.lisp
+++ utilities.lisp
@@ -5,7 +5,7 @@
 ;;; Copyright (c) 2016 aimacode
 
 ;;; Some modify for JSCL and MOREN environment
-;;; Copyright © 2017,2018 Vladimir Mezentsev
+;;; Copyright (C) 2017,2018 Vladimir Mezentsev
 
 
 ;;;; Basic utility functions and macros, used throughout the code.
@@ -55,26 +55,11 @@
         x-y
         (cons x y)))
 
-#|
-"An expression is a list consisting of a prefix operator followed by args,
-Or it can be a symbol, denoting an operator with no arguments.
-Expressions are used in Logic, and as actions for agents."
-
-|#
-
 (defun make-exp (op &rest args) (cons op args))
 (defun op (exp) "Operator of an expression" (if (listp exp) (first exp) exp))
 (defun args (exp) "Arguments of an expression" (if (listp exp) (rest exp) nil))
 (defun arg1 (exp) "First argument" (first (args exp)))
 (defun arg2 (exp) "Second argument" (second (args exp)))
-
-
-#|
-(defsetf args (exp) (new-value)
-  `(setf (cdr ,exp) ,new-value))
-
-|#
-
 
 
 
@@ -91,27 +76,6 @@ Expressions are used in Logic, and as actions for agents."
         (list* (first list) item (insert-between item (rest list)))))
 
 
-;;;; Trivial Functions
-
-(defun nothing (&rest args)
-    "Don't do anything, and return nil."
-    ;;(declare (ignore args))
-    nil)
-
-(defun declare-ignore (&rest args)
-    "Ignore the arguments."
-    ;; This is used to avoid compiler warnings in defmethod.
-    ;; Some compilers warn "Variable unused" if it is bound by a method
-    ;; but does not appear in the body.  However, if you put in a
-    ;; (declare (ignore var)), then other compilers warn "var declared
-    ;; ignored, but is actually used", on the grounds that it is implicitly
-    ;; used to do method dispatch.  So its safest to use declare-ignore.
-    ;; If you like, you can redefine declare-ignore to be a macro that
-    ;; expands to either (declare (ignore args)), or to nothing, depending
-    ;; on the implementation.
-    (values))
-
-
 (defun true (&rest args) "Always return true."  t)
 
 (defun false (&rest args) "Always return false."  nil)
@@ -124,12 +88,6 @@ Expressions are used in Logic, and as actions for agents."
           ((symbolp exp) (symbol-name exp))
           (t (format nil "~A" exp))))
 
-#|
-(defun concat-symbol (&rest args)
-    "Concatenate the args into one string, and turn that into a symbol."
-    (intern (format nil "~{~a~}" args)))
-|#
-
 
 (defun concat-symbol (&rest args)
     "Concatenate the args into one string, and turn that into a symbol."
@@ -140,55 +98,6 @@ Expressions are used in Logic, and as actions for agents."
                             x
                             (if (symbolp x) (symbol-name x)
                                 x))) args))))
-
-
-
-(defun compose (f g)
-    "Return a function h such that (h x) = (f (g x))."
-    #'(lambda (x) (funcall f (funcall g x))))
-
-(defun the-biggest (fn l)
-    (let ((biggest (first l))
-          (best-val (funcall fn (first l))))
-        (dolist (x (rest l))
-            (let ((val (funcall fn x)))
-                (when (> val best-val)
-                    (setq best-val val)
-                    (setq biggest x))))
-        biggest))
-
-(defun the-biggest-random-tie (fn l)
-    (random-element
-     (let ((biggest (list (first l)))
-           (best-val (funcall fn (first l))))
-         (dolist (x (rest l))
-             (let ((val (funcall fn x)))
-                 (cond ((> val best-val)
-                        (setq best-val val)
-                        (setq biggest (list x)))
-                       ((= val best-val)
-                        (push x biggest)))))
-         biggest)))
-
-(defun the-biggest-that (fn p l)
-    (let ((biggest (first l))
-          (best-val (funcall fn (first l))))
-        (dolist (x (rest l))
-            (when (funcall p x)
-                (let ((val (funcall fn x)))
-                    (when (> val best-val)
-                        (setq best-val val)
-                        (setq biggest x)))))
-        biggest))
-
-(defun the-smallest (fn l)
-    (the-biggest (compose #'- fn) l))
-
-(defun the-smallest-random-tie (fn l)
-    (the-biggest-random-tie (compose #'- fn) l))
-
-(defun the-smallest-that (fn p l)
-    (the-biggest-that (compose #'- fn) p l))
 
 
 
